ef1a9815d5719a516983322de7c1ca93d5afcb24
ma-558 <58020381+ma-558@users.noreply.github.com>
2019-12-10 10:33:32 +0900

    マイグレーション変更

--- db/migrate/20191201123132_rename_image_column_to_groups.rb
+++
@@ -1,5 +0,0 @@
-class RenameImageColumnToGroups < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :groups, :image, :name
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20191202081106) do
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
+    t.string   "image",      null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
