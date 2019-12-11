51e49f20764616bf51b655138147d92ce462e159
ma-558 <58020381+ma-558@users.noreply.github.com>
2019-12-11 19:16:15 +0900

    デプロイテスト２

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -15,10 +15,10 @@ pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
