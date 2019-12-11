d552ca50de794890e96fe32b623463737666d6d1
ma-558 <58020381+ma-558@users.noreply.github.com>
2019-12-11 17:52:23 +0900

    デプロイ！！！

--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,10 @@ ActiveRecord::Schema.define(version: 20191202081106) do
   end
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "image",      null: false
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_groups_on_name", unique: true, using: :btree
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
